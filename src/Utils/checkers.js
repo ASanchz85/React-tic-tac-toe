ef16d9f23a825f52e8ca2fe57ecf88acdc47b071
ASanchz85 <asanchz85@gmail.com>
2024-02-23 12:02:02 +0100

    done and working as expected

---
+++ src/Utils/checkers.js
@@ -0,0 +1,21 @@
+import { WINNER_PATTERNS } from "./../constants";
+
+export const checkWinner = (boardToCheck) => {
+  for (const pattern of WINNER_PATTERNS) {
+    const [a, b, c] = pattern;
+
+    if (
+      boardToCheck[a] &&
+      boardToCheck[a] === boardToCheck[b] &&
+      boardToCheck[a] === boardToCheck[c]
+    ) {
+      return boardToCheck[a];
+    }
+  }
+
+  return null;
+};
+
+export const checkEndGame = (boardToCheck) => {
+  return boardToCheck.every((square) => square !== null);
+};
